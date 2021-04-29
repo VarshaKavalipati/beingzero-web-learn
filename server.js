beb05d38f0ca1ce72a1c50af914a739f73963cbf
Varsha <kavalipativarsha2001@gmail.com>
2021-04-29 09:46:30 +0530

    First commit

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
- 
+
 const app = express();
- 
-app.get("/", function(req, res){
+
+app.get("/", function(req, res) {
     res.send("Welcome to Varsha's Basic Site");
 })
- 
+app.get("/resume", function(req, res) {
+    res.send("MY RESUME");
+})
+
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
- 
+
 // Start the server
-app.listen(PORT, function(){
-    console.log("Server Starting running on http://localhost:"+PORT);
+app.listen(PORT, function() {
+    console.log("Server Starting running on http://localhost:" + PORT);
 })
