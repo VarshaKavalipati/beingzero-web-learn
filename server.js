4fb6c2b26d0b2f48e4630da6f1f75c074077cf30
Varsha <kavalipativarsha2001@gmail.com>
2021-04-29 10:06:18 +0530

    Changes again

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 
 const app = express();
-
+//NEW
 app.get("/", function(req, res) {
     res.send("Welcome to Varsha's Basic Site");
 })
-app.get("/resume", function(req, res) {
-    res.send("MY RESUME");
-})
+
 
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
