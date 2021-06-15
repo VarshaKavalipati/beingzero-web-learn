5111353bf1893b7fb7c9e8cf901e5e0c1fa93510
Varsha <kavalipativarsha2001@gmail.com>
2021-06-15 07:32:32 +0530

    session management

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -5,19 +6,65 @@ const bodyParser = require('body-parser');
 const app = express();
 const courselib = require('./backend/lib/courselib');
 const config = require('./backend/config/config');
-const dbConnectLib = require('./backend/lib/dbConnectLib');
+const dbconnect = require('./backend/lib/dbConnectLib');
+const users = require('./backend/models/usermodel');
 // const dbconnect = require('./backend/db/dbconnect');
 
 // dbconnect.connect();
+const { request } = require('express');
+var cookieParser = require("cookie-parser")
+var session = require("express-session");
+const MongoStore = require('connect-mongo');
+dbconnect.connect();
+//dbConnectLib.connect();
 const crypto = require("crypto");
 var todo = [];
+app.use(session({
+    secret: "thi is secret!!!!",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000
+    },
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_CONNECTION_STRING })
+
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/frontend"));
 
 app.use(express.static(path.join(__dirname, "frontend")));
+app.use(cookieParser());
 
-dbConnectLib.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //NEW
@@ -80,6 +127,85 @@ app.post("/crud", courselib.addnewone);
 //const PORT = process.env.PORT || 3000;
 
 // Start the server
+
+
+
+
+
+
+
+
+app.post('/api/login', function(req, res) {
+    users.find(req.body, function(err, data) {
+        if (err) { res.status(400).json({ msg: "Failed" }); } else if (data.length == 1) {
+            req.session.userid = data[0]._id
+            req.session.username = data[0].username
+            console.log(req.session)
+            res.redirect("/indexpage");
+
+        } else {
+
+            res.redirect("/login");
+
+        }
+    });
+})
+var isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.userid)
+        next();
+    else
+        return res.redirect("/login");
+}
+var isNotAuthenticated = (req, res, next) => {
+    if (!req.session || !req.session.userid)
+        next();
+    else
+        return res.redirect("/");
+}
+
+app.get("/indexpage", isAuthenticated, (req, res) => {
+    res.sendFile(__dirname + "/frontend/html/home.html")
+})
+
+app.get("/getdetails", isAuthenticated, (req, res) => {
+    res.json({
+        username: req.session.username
+    });
+})
+
+app.get("/api/logout", isAuthenticated, (req, res) => {
+    req.session.destroy(err => {
+        if (err)
+            return res.status(404).json({
+                err: "error"
+            })
+    })
+
+    return res.status(200).json({
+        message: "succcessfully signout"
+    })
+
+})
+app.post('/api/register', function(req, res) {
+    users.find({ email: req.body.email }, function(err, data) {
+        if (err) { res.status(400).json({ msg: "Failed" }); } else { //console.log(data);
+            if (data.length > 0)
+                res.status(200).json({ msg: "Saved Successful", result: data });
+            else {
+
+                var add = new users(req.body);
+                add.save(function(err, record) {
+                    if (err) {
+                        res.redirect("/register");
+                    } else {
+                        res.redirect("/login");
+                    }
+                });
+            }
+        }
+    });
+})
+
 app.listen(config.webPort, function() {
     console.log("Server Starting running on http://localhost:" + config.webPort);
 })
